7b234656a46d1551fbe85e2e46ab6ae44f004253
Osama Ishaq <osama.ishaq@ibex.co>
2022-02-25 19:47:51 +0500

    first commit

---
+++ src/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+      <div className="p-5 bg-blue-500 text-white text-3xl font-bold">
+        
+        Only the best Spinners
+        {/* <div className="flex">
+          <div className="flex-grow flex">
+            <Link to="/">Fidget Spinner World</Link>
+            <div className="mx-5">|</div>
+            <Link id="cart" to="/cart">
+              Cart
+            </Link>
+          </div>
+          <div className="flex-end relative">
+            <MiniCart />
+            <Login />
+          </div>
+        </div> */}
+      </div>
+    );
+  }
+  
